d0f74cda73854ecc15bf641c87ac8faeea0df377
chibiegg <y-egusa@sakura.ad.jp>
2020-02-12 10:32:18 +0900

    リバースプロキシ時に任意のヘッダを付与できるように

--- config/config.go
+++ config/config.go
@@ -6,15 +6,20 @@ import (
 	"github.com/go-yaml/yaml"
 )
 
+type Upstream struct {
+	URL     string            `yaml:"url"`
+	Headers map[string]string `yaml:"headers"`
+}
+
 type Config struct {
-	ListenAddress string `yaml:"listen_address"`
-	PrivateKeyURL string `yaml:"private_key_url"`
-	CounterURL    string `yaml:"counter_url"`
-	APIKey        string `yaml:"api_key"`
-	Upstream      string `yaml:"upstream"`
-	ProviderName  string `yaml:"provider_name"`
-	ToolLabel     string `yaml:"tool_label"`
-	ToolID        string `yaml:"tool_id"`
+	ListenAddress string   `yaml:"listen_address"`
+	PrivateKeyURL string   `yaml:"private_key_url"`
+	CounterURL    string   `yaml:"counter_url"`
+	APIKey        string   `yaml:"api_key"`
+	Upstream      Upstream `yaml:"upstream"`
+	ProviderName  string   `yaml:"provider_name"`
+	ToolLabel     string   `yaml:"tool_label"`
+	ToolID        string   `yaml:"tool_id"`
 }
 
 func FromFilepath(path string) (*Config, error) {
@@ -27,6 +32,10 @@ func FromFilepath(path string) (*Config, error) {
 	err = yaml.Unmarshal(buf, c)
 	if err != nil {
 		return nil, err
+	}
+
+	if c.Upstream.Headers == nil {
+		c.Upstream.Headers = map[string]string{}
 	}
 
 	return c, err
