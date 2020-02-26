afc03c6c3377f1e39c11a22b9ea079a092217c55
yonda-yonda <minerva.yooda@gmail.com>
2020-02-26 16:12:43 +0900

    本番設定

--- tools/test.py
+++ tools/test.py
@@ -1,8 +1,9 @@
 import requests
 
+
 def get_tool_token(token):
     resp = requests.post(
-        "https://sdk.tellusxdp-staging.com/api/manager/v1/auth/api_access_token/token",
+        "https://sdk.tellusxdp.com/api/manager/v1/auth/api_access_token/token",
         json={"provider_id": "fukuyoshi-jiro", "tool_label": "weather-api"},
         headers={"Authorization": "Bearer {}".format(token)}
     )
@@ -16,7 +17,7 @@ if __name__ == "__main__":
     tool_token = get_tool_token(token)
 
     url = "http://127.0.0.1:8000/demo/wif82af3w39s/api/kaminari_a.php?lat=35.099&lon=138.631&dtime=20190826202000&radi=1.0&format=json"
-    resp = requests.get(url, headers={"Authorization": "Bearer {}".format(tool_token)})
+    resp = requests.get(
+        url, headers={"Authorization": "Bearer {}".format(tool_token)})
     resp.raise_for_status()
     print(resp.text)
-
